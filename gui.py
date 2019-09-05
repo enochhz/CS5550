3839177b1c9810b3a80b9d9b20494109b0c32ea5
walkerzheng <9771330@qq.com>
2019-09-05 15:53:38 -0700

    Updated GUI

--- gui.py
+++ gui.py
@@ -25,7 +25,7 @@ class Window(Frame):
         button_text = "open an image"
         self.open_image_button = Button(self.toolbar, text = button_text, command = self.file_dialog)
         self.open_image_button.pack(side=LEFT, padx=2, pady=2)
-        # edit image button is initially disabled 
+        # edit image button is initially disabled
         self.edit_image_button = Button(self.toolbar, text = "Edit the image", command = self.resize_image)
         self.edit_image_button.pack(side=LEFT, padx=2, pady=2)
         self.edit_image_button.configure(state=DISABLED)
@@ -54,7 +54,7 @@ class Window(Frame):
         img_width, img_height = load.size
         self.img.place(x = (global_width - img_width)/2, y = (global_height - img_height)/2)
         self.edit_image_button.configure(state=NORMAL)
-        
+
 def main():
     root = Tk()
     app = Window(root)
